4d69b5c8d04fb510f2f593861de9601848cacdca
Miriam  Lehmacher <83802@b5-420-19.ict.lab.locals>
2020-01-21 11:21:29 +0100

    original

---
+++ javascript/fade.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#fade').fadeIn(1500);
+    $('ul').fadeIn(1500);
+    $('#miri').fadeIn(1500);
+    $('h1').fadeIn(1500);
+    $('h2').fadeIn(2500);
+    $('button').fadeIn(3100);
+    $('.text').fadeIn(3100);
+    $('.homeIcon').fadeIn(3100);
+});
